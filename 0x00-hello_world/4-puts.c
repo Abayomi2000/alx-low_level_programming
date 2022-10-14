1d91e5fe74c30bec207f8122fcb532c1baf11a1c
Abayomi2000 <ayoolaabayomi0@gmail.com>
2022-10-13 19:04:57 -0700

    great

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  * Return: 0 if success.
  */
 int main(void)
-{
-  puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+{	
+         puts("\"Programming is like building a multilingual puzzle");
+	 return (0);
 }	
