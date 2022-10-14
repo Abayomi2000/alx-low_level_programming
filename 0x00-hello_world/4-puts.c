1f3e477f3dd6ba1336aea4781f799c287173c365
Abayomi2000 <ayoolaabayomi0@gmail.com>
2022-10-14 14:05:19 -0700

    my first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-
 #include <stdio.h>
 
 /**
- * main - printing programs
- * Return: 0 if successful
+ *Discription: main - prints programming
+ *Return: 0 if successful
+ */
 int main(void)
 {
-          puts("\"Programming is like building a multilingual puzzle");
-          return (0);
+	puts("Programming is like building a multilingual puzzle,/n");
+	return (0);
 }
