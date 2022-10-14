6c9c28180b273b3624d594b3a4e4bfba09b0e8fe
Abayomi2000 <ayoolaabayomi0@gmail.com>
2022-10-13 18:06:03 -0700

    commit file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
- /**
-  * main - This prints a multilinqual puzzle
-  *
-  *Return: Always 0 (successful)
-  */
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
 int main(void)
-{ 
+{
 	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	return (0);
 }	
