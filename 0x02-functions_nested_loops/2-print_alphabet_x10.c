ced4253b02e5e39f3f6fa698452b3da430e69da3
Abayomi2000 <ayoolaabayomi0@gmail.com>
2022-10-21 03:15:54 -0700

    my commit message

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,7 +12,7 @@ void print_alphabet_x10(void);
 
 	while (count++ <= 9)
 	{
-		for (letter = 'a'; letter <= 'z; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 		_putchar('\n');
 	}
