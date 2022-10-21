a45a234aa080f2a4eb633d8d739b99c40588806d
Abayomi2000 <ayoolaabayomi0@gmail.com>
2022-10-21 11:20:35 -0700

    my commit message

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,15 @@
 
 /**
  * _islower - check the code in lowercase
- * @c: is the code to check for
- * return: alwas 0
+ * @c: char test
+ * Return: 1 if it is lower case otherwise 0
  */
-
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
+	{
+		if (c >= 'a' && c <= 'z')
+			return (1);
+	}
+	return (0);
 		return (0);
 }
