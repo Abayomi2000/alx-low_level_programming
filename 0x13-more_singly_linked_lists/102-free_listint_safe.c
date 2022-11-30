8f59c34ce488efcb013b10a04f0218368573e61b
Abayomi2000 <ayoolaabayomi0@gmail.com>
2022-11-29 16:58:45 -0800

    commit my work

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+  * free_listint_safe - define function
+  * @h:
+  * Return: what does it return?
+  */
+size_t free_listint_safe(listint_t **h)
+{
+	h++;
+	return (0);
+}
+
---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+  * find_listint_loop - define function
+  * @head:
+  * Return: what does it return?
+  */
+listint_t *find_listint_loop(listint_t *head)
+{
+	head++;
+	return (0);
+}
+
